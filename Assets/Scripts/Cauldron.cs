4e2a78cf3fe5eabe003114118b039a18e316c14e
TheCrimsonAxe <thecrimsonaxe@gmail.com>
2026-02-19 14:41:58 -0600

    new plants and recipe foundation

--- Assets/Scripts/Cauldron.cs
+++ Assets/Scripts/Cauldron.cs
@@ -6,7 +6,6 @@ public class Cauldron : MonoBehaviour
     public GameObject fire;
     private void OnTriggerEnter(Collider other)
     {
-        //if it's a water spell, soil becomes wet
         if (other.CompareTag("FireSpell"))
         {
             fire.SetActive(true);
