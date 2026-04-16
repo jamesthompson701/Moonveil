9cb5a7766b1a97dcf78e1d4d6e0848ae8c6923b5
James Thompson <james.thompson7@g.austincc.edu>
2026-04-16 00:28:20 -0500

    Cutscene now plays

--- Assets/Scripts/Canvas/TitleScreen.cs
+++ Assets/Scripts/Canvas/TitleScreen.cs
@@ -5,6 +5,7 @@ public class TitleScreen : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(1);
+        VideoManager.Instance.PlayVideo(eVideos.cutscene);
+        Destroy(gameObject);
     }
 }
