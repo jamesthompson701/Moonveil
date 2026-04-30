cd99a048eafa7617cd7da3f48b6dc80cdf7613af
TheCrimsonAxe <thecrimsonaxe@gmail.com>
2026-04-30 14:56:58 -0500

    World Tree progress
    this is more complicated than i thought

--- Assets/Scriptable Objects/SO Scripts/TreeQuestSO.cs
+++ Assets/Scriptable Objects/SO Scripts/TreeQuestSO.cs
@@ -6,24 +6,26 @@ public class TreeQuestSO : ScriptableObject
     // how many total items are needed to fulfill this quest
     public int questGoal;
 
-    // tree quests can demand up to 5 different items, and any number of each of them
-
-    // this int counts how many kinds of items it needs (up to 5)
+    // tree quests can demand up to 6 different items, and any number of each of them
+    // this int counts how many kinds of items it needs
     public int numOfItems;
 
     // the items needed and how many of each of them
     public ItemSO item1;
-    public int item1Count;
+    public int item1Needed;
 
     public ItemSO item2;
-    public int item2Count;
+    public int item2Needed;
 
     public ItemSO item3;
-    public int item3Count;
+    public int item3Needed;
 
     public ItemSO item4;
-    public int item4Count;
+    public int item4Needed;
 
     public ItemSO item5;
-    public int item5Count;
+    public int item5Needed;
+
+    public ItemSO item6;
+    public int item6Needed;
 }
--- Assets/Scripts/WorldTree.cs
+++ Assets/Scripts/WorldTree.cs
@@ -1,12 +1,73 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class WorldTree : MonoBehaviour
 {
+    //menu
     public GameObject myMenu;
+
+    //current quest
+    private TreeQuestSO curQuest;
+
+    //quest progress on each item
+    private int item1Count;
+    private int item2Count;
+    private int item3Count;
+    private int item4Count;
+    private int item5Count;
+    private int item6Count;
+
+    //list of quests
+    public TreeQuestSO[] quests;
+
+    // currently chosen item
+    public Image currentlySelectedImage;
+    public ItemSO currentlySelected;
+
+
+    //everything to do with the quest item widgets
+    public GameObject questItemWidget;
+    public GameObject itemsMenu;
+
+    public void Awake()
+    {
+        curQuest = quests[0];
+    }
 
     public virtual void OnInteract()
     {
         myMenu.SetActive(true);
     }
 
+    public void ItemClicked(ItemSO _item)
+    {
+        currentlySelectedImage.sprite = _item.itemSprite;
+        currentlySelected = _item;
+    }
+
+    public void ItemDeposited()
+    {
+        //when the deposit button is clicked, check each of the quest items to see which one matches the deposited item
+        //increase the count for that item and, if the count matches how many are needed, delete that from the menu
+        //Finally, check the item counts again. If every item on the menu is at max, move on to the next quest
+        if (curQuest.item1 == currentlySelected)
+        {
+            item1Count++;
+            if (item1Count == curQuest.item1Needed)
+            {
+
+            }
+        }
+    }
+
+    public void GenerateQuestItemWidget(ItemSO _item)
+    {
+        GameObject questWidget = Instantiate(questItemWidget);
+        wQuestItem questItem = questWidget.GetComponent<wQuestItem>();
+        questWidget.transform.SetParent(itemsMenu.transform);
+        questItem.myItem = _item;
+        questItem.myImage.sprite = _item.itemSprite;
+        questItem.Refresh();
+    }
 }
