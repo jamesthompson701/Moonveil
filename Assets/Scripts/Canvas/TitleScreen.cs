0050ff35be6821dccfb92a28fa07360e08dac997
gdli21 <grace.li3@g.austincc.edu>
2026-06-08 15:03:21 -0500

    Changed Title Screen

--- Assets/Scripts/Canvas/TitleScreen.cs
+++ Assets/Scripts/Canvas/TitleScreen.cs
@@ -3,6 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class TitleScreen : MonoBehaviour
 {
+
+    [SerializeField] private GameObject optionsCanvas;
+    [SerializeField] private GameObject creditsCanvas;
+
     private void Start()
     {
         Cursor.lockState = CursorLockMode.None;
@@ -15,5 +19,26 @@ public class TitleScreen : MonoBehaviour
         SceneManager.LoadScene(1);
         //VideoManager.Instance.PlayVideo(eVideos.cutscene);
         //Destroy(gameObject);
+    }
+
+    public void OpenOptions()
+    {
+        Debug.Log("Options Button Clicked");
+        
+        optionsCanvas.SetActive(true);
+        optionsCanvas.transform.SetAsLastSibling();
+    }
+
+    public void OpenCredits()
+    {
+        Debug.Log("Credits Button Clicked");
+
+        creditsCanvas.SetActive(true);
+        creditsCanvas.transform.SetAsLastSibling();
+    }
+
+    public void CloseOptions()
+    {
+        optionsCanvas.SetActive(false);
     }
 }
