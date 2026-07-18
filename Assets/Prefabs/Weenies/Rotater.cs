3839a99ed321625642db938b7715f8519f63af30
Tyler <tylerhaxton96@gmail.com>
2026-07-18 14:55:01 -0500

    spin
    gem go speeeen

--- Assets/Prefabs/Weenies/Rotater.cs
+++ Assets/Prefabs/Weenies/Rotater.cs
@@ -2,8 +2,13 @@ using UnityEngine;
 
 public class Rotater : MonoBehaviour
 {
+    public float spinSpeed = 20f;
+    public bool isVertical = true;
     void Update()
     {
-        transform.Rotate(Vector3.up, 20 * Time.deltaTime);
+        if (isVertical)
+            transform.Rotate(Vector3.up, spinSpeed * Time.deltaTime);
+        else 
+            transform.Rotate(Vector3.forward, spinSpeed * Time.deltaTime);
     }
 }
